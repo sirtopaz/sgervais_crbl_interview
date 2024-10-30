6553095534c0c8cca936331f39ff4748d1409898
Sean <github@virtualsean.com>
2024-10-30 18:35:16 -0500

    more placeholders

---
+++ src/component/action/IconButton.tsx
@@ -0,0 +1,7 @@
+import { type FC } from "react";
+
+const IconButton: FC = () => {
+  return null;
+};
+
+export default IconButton;
---
+++ src/component/action/index.ts
@@ -0,0 +1,3 @@
+export { default as IconButton } from "react";
+
+// TODO think about creating a toggle button as a higher order component for icon button
