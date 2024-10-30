c7f38a641d7650553329a40478d477282061d533
Sean <github@virtualsean.com>
2024-10-30 12:20:29 -0500

    updated vitest setup

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
-// TODO LATER - this is showing as incorrect syntax but doesn't seem to affect the test functionaltiy
 export default defineConfig({
   plugins: [react()],
   test: {
     environment: "jsdom",
     globals: true,
-    setupFiles: "./vitest-setup.js",
+    setupFiles: "./vitest-setup.ts",
   },
 });
