69ea78d67e4cb9b95bc896576fecfe6cf79c17d6
Sean <github@virtualsean.com>
2024-10-31 11:57:48 -0500

    WIP - progress on log data provider

--- src/common/data/LogDataProvider.tsx
+++ src/common/data/LogDataProvider.tsx
@@ -1,7 +1,8 @@
-import { useState, type FC, type ReactNode } from "react";
+import { useEffect, useState, type FC, type ReactNode } from "react";
 
 import { LoadingStatus } from "./data.constants";
-import { logEvent } from "./data.types";
+import { LogDataState, LogEvent } from "./data.types";
+import { NdjsonStreamLoader, LogDataContext } from "./data.helper";
 
 interface LogDataProviderProps {
   url: string; // url for logs
@@ -10,12 +11,36 @@ interface LogDataProviderProps {
 
 const LogDataProvider: FC<LogDataProviderProps> = ({ url, children }) => {
   const [status, setStatus] = useState<LoadingStatus>(LoadingStatus.START);
-  const [logEvents] = useState<logEvent[]>([]);
+  const [logEvents, setLogEvents] = useState<LogEvent[]>([]); // TODO convert to reducer?? to make adding events to list easier
   const [logEventCount, setLogEventCount] = useState(0);
 
-  //TODO - setup stream loader, provider and context
+  useEffect(() => {
+    NdjsonStreamLoader(url)
+      .then((stream) => {
+        setLogEventCount(0);
+        setLogEvents([]);
 
-  return <>{children}</>;
+        //TODO as reading stream, convert to event, update event count
+
+        stream.getReader();
+      })
+      .catch(() => {
+        // NOTE - IRL log the error to a service or console for debugging
+        setStatus(LoadingStatus.ERROR);
+      });
+  }, [url]);
+
+  const currentLogDataState: LogDataState = {
+    logEventCount,
+    logEvents,
+    status,
+  };
+
+  return (
+    <LogDataContext.Provider value={currentLogDataState}>
+      {children}
+    </LogDataContext.Provider>
+  );
 };
 
 export default LogDataProvider;
