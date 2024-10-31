a10ac376ba9d81c82f3310a42daf94cd040a60f9
Sean <github@virtualsean.com>
2024-10-31 11:58:47 -0500

    wireframing log data table display component

--- src/component/log/LogEventTable.tsx
+++ src/component/log/LogEventTable.tsx
@@ -1,7 +1,50 @@
 import { type FC } from "react";
 
+import "./LogEventTable.scss";
+
+import { LogEvent, useLogDataState } from "../../common/data";
+
+// TODO add toggle state and display styling
+const LogEventRow: FC<LogEvent> = ({ json, time }) => {
+  return (
+    <tr className="log-event-row">
+      <td className="toggle">ICON</td>
+      <td className="event-time">{new Date(time).toISOString()}</td>
+      <td className="event-json">{json}</td>
+    </tr>
+  );
+};
+
 const LogEventTable: FC = () => {
-  return <div>Coming Soon</div>;
+  const { logEventCount, logEvents, status } = useLogDataState();
+
+  return (
+    <div className="log-event-table">
+      <table>
+        <thead>
+          <tr>
+            <th scope="col">
+              <span className="sr-only">Toggle Event Details</span>
+            </th>
+            <th scope="col">Time</th>
+            <th scope="col">Event</th>
+          </tr>
+        </thead>
+        <tbody>
+          {logEvents.map((event, idx) => (
+            <LogEventRow key={`event_${idx}`} {...event} />
+          ))}
+        </tbody>
+        <tfoot>
+          <tr>
+            <td>&nbsp;</td>
+            <td>{status}</td>
+            <td>Event Count: {logEventCount}</td>
+          </tr>
+        </tfoot>
+      </table>
+    </div>
+  );
 };
 
 export default LogEventTable;
